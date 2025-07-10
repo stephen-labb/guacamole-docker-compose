ea0bb046ceeec79859880bc61e22c6f0d756e668
Boschkundendienst <boschkundendienst@users.noreply.github.com>
2025-07-10 15:21:56 +0200

    Update prepare.sh
    pinned version for guacamole/guacamole in docker run command

--- prepare.sh
+++ prepare.sh
@@ -10,7 +10,10 @@ echo "Preparing folder init and creating ./init/initdb.sql"
 mkdir ./init >/dev/null 2>&1
 mkdir -p ./nginx/ssl >/dev/null 2>&1
 chmod -R +x ./init
-docker run --rm guacamole/guacamole /opt/guacamole/bin/initdb.sh --postgresql > ./init/initdb.sql
+# latest
+#docker run --rm 'guacamole/guacamole' /opt/guacamole/bin/initdb.sh --postgresql > ./init/initdb.sql
+# pinned version
+docker run --rm 'guacamole/guacamole:1.6.0' /opt/guacamole/bin/initdb.sh --postgresql > ./init/initdb.sql
 echo "done"
 echo "Preparing folder record and set permissions"
 mkdir ./record >/dev/null 2>&1
